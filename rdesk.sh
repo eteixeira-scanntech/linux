b0db81e02469667725d1c684084f9aa66dbcfd2f
eteixeira-scanntech <eteixeira.scanntech@gmail.com>
2026-03-12 17:40:30 -0300

    Update rdesk.sh

--- rdesk.sh
+++ rdesk.sh
@@ -10,6 +10,7 @@ cd "$TMPDIR"
 wget https://github.com/rustdesk/rustdesk/releases/download/1.4.6/rustdesk-1.4.6-x86_64.deb
 
 sudo apt update
+sudo apt install -y libgtk-3-0 libxcb-randr0 libxdo3 libxfixes3 libxcb-shape0 libxcb-xfixes0
 sudo apt install -y ./rustdesk-*.deb
 
 echo "RustDesk instalado."
